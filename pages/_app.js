9d562d2426c38ddca7c5d7340607449ec036befd
karunakarpatel-kp <karunakarpatel.kp@gmail.com>
2023-03-21 17:21:17 +0530

    Added Google Anayltics Code For Testing

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,23 @@
 import { ThemeProvider } from "@emotion/react";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
 import "../styles/globals.css";
 import "../public/fonts/font.css";
 
+import { initGA, logPageView } from "../Components/SEO/Analytics";
+
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
   const getLayout = Component.getLayout || ((page) => page);
-  return getLayout(<Component {...pageProps} />);
+  useEffect(() => {
+    initGA();
+    logPageView();
+    router.events.on("routeChangeComplete", logPageView);
+    return () => {
+      router.events.off("routeChangeComplete", logPageView);
+    };
+  }, []);
+
+  return <>{getLayout(<Component {...pageProps} />)}</>;
 }
--- pages/index.jsx
+++ pages/index.jsx
@@ -14,56 +14,23 @@ import coding from "../public/coding.jpg";
 import hi from "../public/hi.jpg";
 import Link from "next/link";
 import { themeColors } from "../Components/Theme/Theme";
+import HeaderSection from "../Components/SEO/Head";
 
 const Home = () => {
   return (
     <>
+      <HeaderSection
+        title="Home Page"
+        description="This is the Description of the Home Page"
+        image="https://image-url"
+        url="https://blog-page-url"
+        publishedTime="2023-01-07T21:58:00+00:00"
+        lastUpdatedTime="2023-01-07T21:58:00+00:00"
+      />
+
       <Box textAlign="center" mb={3}>
         <Typography variant="h6">Recent Articles</Typography>
       </Box>
-      {/* <Grid container sx={{ spacing: { xs: 0, sm: 2, md: 2, lg: 2 }, pl: { xs: 1 } }}>
-        <Grid item xs={12} sm={4} md={4} lg={4}>
-          <ImageOnTopCard
-            Title="Learn seo from stasd rt to finish in this blog post"
-            Description="In this blog post you will be able o learn all the required technologies and most often you will able to see all the new era of technologies in this this blog post you will be able o learn all the required technologies and most often you will able to see all the new era of technologies in this World this blog post you will be able o learn all the required technologies and most often you will able to see all the new era of technologies in this World World.....!"
-            href="/Blog/seo"
-            ImageSrc={hi}
-            ImageAlt="hi"
-            tags={[
-              { tag: "seo", href: "/Blog/seo" },
-              { tag: "material ui", href: "/mui" },
-            ]}
-          />
-        </Grid>
-        <Grid item xs={12} sm={4} md={4} lg={4}>
-          <ImageOnTopCard
-            Title="Learn SEO Right From Start To Finish Image Side"
-            Description="Learn how to create and customize a Multiselect component with Material UI and React"
-            href="/Blog/seo"
-            ImageSrc={seo}
-            ImageAlt="Coding-Image"
-            tags={[
-              { tag: "seo", href: "/Blog/seo" },
-              { tag: "material ui", href: "/mui" },
-              { tag: "material ui", href: "/mui" },
-            ]}
-          />
-        </Grid>
-        <Grid item xs={12} sm={4} md={4} lg={4}>
-          <ImageOnTopCard
-            Title="Learn SEO Right From Start To Finish Image On Right Side"
-            Description="In this blog post you will be able o learn all the required technologies and most often you will able to see all the new era of technologies in this this blog post you will be able o learn all the required technologies and most often you will able to see all the new era of technologies in this World this blog post you will be able o learn all the required technologies and most often you will able to see all the new era of technologies in this World World.....!"
-            href="/Blog/seo"
-            ImageSrc={coding}
-            ImageAlt="Coding-Image"
-            tags={[
-              { tag: "seo", href: "/Blog/seo" },
-              { tag: "material ui", href: "/mui" },
-              { tag: "material ui", href: "/mui" },
-            ]}
-          />
-        </Grid>
-      </Grid> */}
 
       <Grid container sx={{ spacing: { xs: 0, sm: 2, md: 2, lg: 2 }, pl: { xs: 1 } }}>
         <Grid item xs={12} sm={12} md={4} lg={4}>
@@ -129,6 +96,50 @@ const Home = () => {
           <ArrowForwardIcon fontSize="small" sx={{ ml: "2px" }} />
         </Link>
       </Box>
+
+      {/* <Grid container sx={{ spacing: { xs: 0, sm: 2, md: 2, lg: 2 }, pl: { xs: 1 } }}>
+        <Grid item xs={12} sm={4} md={4} lg={4}>
+          <ImageOnTopCard
+            Title="Learn seo from stasd rt to finish in this blog post"
+            Description="In this blog post you will be able o learn all the required technologies and most often you will able to see all the new era of technologies in this this blog post you will be able o learn all the required technologies and most often you will able to see all the new era of technologies in this World this blog post you will be able o learn all the required technologies and most often you will able to see all the new era of technologies in this World World.....!"
+            href="/Blog/seo"
+            ImageSrc={hi}
+            ImageAlt="hi"
+            tags={[
+              { tag: "seo", href: "/Blog/seo" },
+              { tag: "material ui", href: "/mui" },
+            ]}
+          />
+        </Grid>
+        <Grid item xs={12} sm={4} md={4} lg={4}>
+          <ImageOnTopCard
+            Title="Learn SEO Right From Start To Finish Image Side"
+            Description="Learn how to create and customize a Multiselect component with Material UI and React"
+            href="/Blog/seo"
+            ImageSrc={seo}
+            ImageAlt="Coding-Image"
+            tags={[
+              { tag: "seo", href: "/Blog/seo" },
+              { tag: "material ui", href: "/mui" },
+              { tag: "material ui", href: "/mui" },
+            ]}
+          />
+        </Grid>
+        <Grid item xs={12} sm={4} md={4} lg={4}>
+          <ImageOnTopCard
+            Title="Learn SEO Right From Start To Finish Image On Right Side"
+            Description="In this blog post you will be able o learn all the required technologies and most often you will able to see all the new era of technologies in this this blog post you will be able o learn all the required technologies and most often you will able to see all the new era of technologies in this World this blog post you will be able o learn all the required technologies and most often you will able to see all the new era of technologies in this World World.....!"
+            href="/Blog/seo"
+            ImageSrc={coding}
+            ImageAlt="Coding-Image"
+            tags={[
+              { tag: "seo", href: "/Blog/seo" },
+              { tag: "material ui", href: "/mui" },
+              { tag: "material ui", href: "/mui" },
+            ]}
+          />
+        </Grid>
+      </Grid> */}
 
       {/* <ImageOnRightCard
         Title="Learn SEO Right From Start To Finish Image On Right Side"
