a10cef9c48a288e0986b28c7b2c5fe4de6f89f35
Karunakar Patel <karunakarpatel.kp@gmail.com>
2024-02-07 17:57:56 +0530

    Entered ENV Variables in Vercel Production, so new deployment is required, made small changes and pushed again

--- next.config.js
+++ next.config.js
@@ -4,7 +4,6 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   env: {
-    PIXELS_API_KEY: process.env.PIXELS_API_KEY,
     HOME_PAGE_BASE_URL: process.env.HOME_PAGE_BASE_URL,
     BLOG_PAGE_BASE_URL: process.env.BLOG_PAGE_BASE_URL,
   },
