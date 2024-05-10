10c6e0566777f6758bfe9a039daa10948da41704
Karunakar Patel <karunakarpatel.kp@gmail.com>
2024-05-10 13:22:42 +0530

    Changed and replaced the actual karunakar Patel Logo in the desktop & mobile version

--- Components/NavigationBar/Navigation.tsx
+++ Components/NavigationBar/Navigation.tsx
@@ -54,46 +54,78 @@ const Navigation = () => {
           className="col-span-9 md:col-span-4 border-0 border-green-800 mt-1 ml-0 cursor-pointer"
           onClick={onLogoClickHandler}
         >
-          <svg xmlns="http://www.w3.org/2000/svg" width="200" height="48" viewBox="0 0 345 88">
-            <path
-              id="Path_7"
-              data-name="Path 7"
-              d="M64,5H10A10,10,0,0,0,0,15V62A10,10,0,0,0,10,72H64A10,10,0,0,0,74,62V15A10,10,0,0,0,64,5Z"
-              transform="translate(0 11)"
-              fill="#ffca3c"
-            />
-            <text
-              id="Text_2_Image"
-              data-name="Karunakar Patel"
-              transform="translate(89)"
-              fill="#ffca3c"
-              fontSize="35"
-              fontFamily="SegoeUI-Semibold, Segoe UI"
-              fontWeight="600"
-            >
-              <tspan x="0" y="40">
-                Karunakar
-              </tspan>
-            </text>
-            <text
-              id="Converter"
-              transform="translate(89 79)"
-              fill="#ffca3c"
-              fontSize="35"
-              fontFamily="SegoeUI-Semibold, Segoe UI"
-              fontWeight="600"
-            >
-              <tspan x="0" y="0">
-                Patel
-              </tspan>
-            </text>
-            <path
-              id="Path_8"
-              data-name="Path 8"
-              d="M57.441,14.809A2.014,2.014,0,0,1,56.872,17l-3.849,3.5a16.943,16.943,0,0,1,0,4.516l3.849,3.5a2.014,2.014,0,0,1,.569,2.187,22.034,22.034,0,0,1-1.4,3.049l-.418.72a22.619,22.619,0,0,1-1.965,2.773,2.013,2.013,0,0,1-2.178.6l-4.951-1.573a17.1,17.1,0,0,1-3.911,2.258L41.5,43.61a2.009,2.009,0,0,1-1.618,1.582,23.093,23.093,0,0,1-7.556,0,2.009,2.009,0,0,1-1.618-1.582L29.6,38.535a17.1,17.1,0,0,1-3.911-2.258l-4.942,1.582a2.029,2.029,0,0,1-2.178-.6A22.62,22.62,0,0,1,16.6,34.481l-.418-.72a22.034,22.034,0,0,1-1.4-3.049,2.014,2.014,0,0,1,.569-2.187l3.849-3.5a17.44,17.44,0,0,1-.151-2.267A17.2,17.2,0,0,1,19.2,20.5L15.35,17a2.014,2.014,0,0,1-.569-2.187,22.034,22.034,0,0,1,1.4-3.049l.418-.72a22.619,22.619,0,0,1,1.965-2.773,2.013,2.013,0,0,1,2.178-.6L25.7,9.236a17.105,17.105,0,0,1,3.911-2.258L30.72,1.9A2.009,2.009,0,0,1,32.337.32,22.037,22.037,0,0,1,36.115,0a22.969,22.969,0,0,1,3.778.311,2.009,2.009,0,0,1,1.618,1.582l1.111,5.076a17.1,17.1,0,0,1,3.911,2.258l4.951-1.573a2.029,2.029,0,0,1,2.178.6,22.62,22.62,0,0,1,1.965,2.773l.418.72a22.033,22.033,0,0,1,1.4,3.049ZM36.115,29.868A7.111,7.111,0,1,0,29,22.756a7.111,7.111,0,0,0,7.111,7.111Z"
-              transform="translate(1.338 27)"
-              fill="black"
-            />
+          <svg xmlns="http://www.w3.org/2000/svg" width="200" height="48" viewBox="0 0 287 83">
+            <g id="Group_39" data-name="Group 39" transform="translate(-370 -18)">
+              <text
+                id="Karunakar_"
+                data-name="Karunakar "
+                transform="translate(459 50)"
+                fill="#ffca3e"
+                font-size="35"
+                font-family="Georgia-Bold, Georgia"
+                font-weight="700"
+              >
+                <tspan x="0" y="0">
+                  Karunakar
+                </tspan>
+              </text>
+              <text
+                id="Patel_"
+                data-name="Patel "
+                transform="translate(459 93)"
+                fill="#ffca3e"
+                font-size="35"
+                font-family="Georgia-Bold, Georgia"
+                font-weight="700"
+              >
+                <tspan x="0" y="0">
+                  Patel
+                </tspan>
+              </text>
+              <rect
+                id="Rectangle_3"
+                data-name="Rectangle 3"
+                width="74"
+                height="67"
+                rx="10"
+                transform="translate(370 27)"
+                fill="#ffca3e"
+              />
+              <g id="Group_5" data-name="Group 5" transform="translate(387 45)">
+                <line
+                  id="Line_3"
+                  data-name="Line 3"
+                  x1="6.457"
+                  y2="31.734"
+                  transform="translate(18.605)"
+                  fill="none"
+                  stroke="#284a98"
+                  stroke-width="5"
+                />
+                <g id="Group_4" data-name="Group 4" transform="translate(0 8.071)">
+                  <path
+                    id="Path_6"
+                    data-name="Path 6"
+                    d="M16,26.488,28.562,16.244,16,6"
+                    transform="translate(13.311 -6)"
+                    fill="none"
+                    stroke="#284a98"
+                    stroke-linejoin="round"
+                    stroke-width="5"
+                  />
+                  <path
+                    id="Path_7"
+                    data-name="Path 7"
+                    d="M14.562,6,2,16.244,14.562,26.488"
+                    transform="translate(-2 -6)"
+                    fill="none"
+                    stroke="#284a98"
+                    stroke-linejoin="round"
+                    stroke-width="5"
+                  />
+                </g>
+              </g>
+            </g>
           </svg>
         </div>
         <div className="hidden md:block col-span-6 border-0 border-purple-400 m-0 p-0 ">
@@ -148,46 +180,78 @@ const Navigation = () => {
           className="mt-1 ml-0 cursor-pointer p-1 pb-4 mb-4 border-b border-b-slate-700"
           onClick={onLogoClickHandler}
         >
-          <svg xmlns="http://www.w3.org/2000/svg" width="200" height="48" viewBox="0 0 345 88">
-            <path
-              id="Path_7"
-              data-name="Path 7"
-              d="M64,5H10A10,10,0,0,0,0,15V62A10,10,0,0,0,10,72H64A10,10,0,0,0,74,62V15A10,10,0,0,0,64,5Z"
-              transform="translate(0 11)"
-              fill="#ffca3c"
-            />
-            <text
-              id="Text_2_Image"
-              data-name="Karunakar Patel"
-              transform="translate(89)"
-              fill="#ffca3c"
-              fontSize="35"
-              fontFamily="SegoeUI-Semibold, Segoe UI"
-              fontWeight="600"
-            >
-              <tspan x="0" y="40">
-                Karunakar
-              </tspan>
-            </text>
-            <text
-              id="Converter"
-              transform="translate(89 79)"
-              fill="#ffca3c"
-              fontSize="35"
-              fontFamily="SegoeUI-Semibold, Segoe UI"
-              fontWeight="600"
-            >
-              <tspan x="0" y="0">
-                Patel
-              </tspan>
-            </text>
-            <path
-              id="Path_8"
-              data-name="Path 8"
-              d="M57.441,14.809A2.014,2.014,0,0,1,56.872,17l-3.849,3.5a16.943,16.943,0,0,1,0,4.516l3.849,3.5a2.014,2.014,0,0,1,.569,2.187,22.034,22.034,0,0,1-1.4,3.049l-.418.72a22.619,22.619,0,0,1-1.965,2.773,2.013,2.013,0,0,1-2.178.6l-4.951-1.573a17.1,17.1,0,0,1-3.911,2.258L41.5,43.61a2.009,2.009,0,0,1-1.618,1.582,23.093,23.093,0,0,1-7.556,0,2.009,2.009,0,0,1-1.618-1.582L29.6,38.535a17.1,17.1,0,0,1-3.911-2.258l-4.942,1.582a2.029,2.029,0,0,1-2.178-.6A22.62,22.62,0,0,1,16.6,34.481l-.418-.72a22.034,22.034,0,0,1-1.4-3.049,2.014,2.014,0,0,1,.569-2.187l3.849-3.5a17.44,17.44,0,0,1-.151-2.267A17.2,17.2,0,0,1,19.2,20.5L15.35,17a2.014,2.014,0,0,1-.569-2.187,22.034,22.034,0,0,1,1.4-3.049l.418-.72a22.619,22.619,0,0,1,1.965-2.773,2.013,2.013,0,0,1,2.178-.6L25.7,9.236a17.105,17.105,0,0,1,3.911-2.258L30.72,1.9A2.009,2.009,0,0,1,32.337.32,22.037,22.037,0,0,1,36.115,0a22.969,22.969,0,0,1,3.778.311,2.009,2.009,0,0,1,1.618,1.582l1.111,5.076a17.1,17.1,0,0,1,3.911,2.258l4.951-1.573a2.029,2.029,0,0,1,2.178.6,22.62,22.62,0,0,1,1.965,2.773l.418.72a22.033,22.033,0,0,1,1.4,3.049ZM36.115,29.868A7.111,7.111,0,1,0,29,22.756a7.111,7.111,0,0,0,7.111,7.111Z"
-              transform="translate(1.338 27)"
-              fill="black"
-            />
+          <svg xmlns="http://www.w3.org/2000/svg" width="200" height="48" viewBox="0 0 287 83">
+            <g id="Group_39" data-name="Group 39" transform="translate(-370 -18)">
+              <text
+                id="Karunakar_"
+                data-name="Karunakar "
+                transform="translate(459 50)"
+                fill="#ffca3e"
+                font-size="35"
+                font-family="Georgia-Bold, Georgia"
+                font-weight="700"
+              >
+                <tspan x="0" y="0">
+                  Karunakar
+                </tspan>
+              </text>
+              <text
+                id="Patel_"
+                data-name="Patel "
+                transform="translate(459 93)"
+                fill="#ffca3e"
+                font-size="35"
+                font-family="Georgia-Bold, Georgia"
+                font-weight="700"
+              >
+                <tspan x="0" y="0">
+                  Patel
+                </tspan>
+              </text>
+              <rect
+                id="Rectangle_3"
+                data-name="Rectangle 3"
+                width="74"
+                height="67"
+                rx="10"
+                transform="translate(370 27)"
+                fill="#ffca3e"
+              />
+              <g id="Group_5" data-name="Group 5" transform="translate(387 45)">
+                <line
+                  id="Line_3"
+                  data-name="Line 3"
+                  x1="6.457"
+                  y2="31.734"
+                  transform="translate(18.605)"
+                  fill="none"
+                  stroke="#284a98"
+                  stroke-width="5"
+                />
+                <g id="Group_4" data-name="Group 4" transform="translate(0 8.071)">
+                  <path
+                    id="Path_6"
+                    data-name="Path 6"
+                    d="M16,26.488,28.562,16.244,16,6"
+                    transform="translate(13.311 -6)"
+                    fill="none"
+                    stroke="#284a98"
+                    stroke-linejoin="round"
+                    stroke-width="5"
+                  />
+                  <path
+                    id="Path_7"
+                    data-name="Path 7"
+                    d="M14.562,6,2,16.244,14.562,26.488"
+                    transform="translate(-2 -6)"
+                    fill="none"
+                    stroke="#284a98"
+                    stroke-linejoin="round"
+                    stroke-width="5"
+                  />
+                </g>
+              </g>
+            </g>
           </svg>
         </div>
 
