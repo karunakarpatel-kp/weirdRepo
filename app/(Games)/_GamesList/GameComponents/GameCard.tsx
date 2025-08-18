a8c313571ce4b50261d9f68f33b9da9827d669d8
Karunakar_Patel <karunakarpatel.kp@gmail.com>
2025-08-18 10:13:35 +0530

    Added some ui to home page

---
+++ app/(Games)/_GamesList/GameComponents/GameCard.tsx
@@ -0,0 +1,32 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+interface GameCardProps {
+  linkHref: string;
+  linkTitle: string;
+  imgSrc: string | any;
+  imgAlt: string;
+  gameTitle: string;
+  scalVal?: number;
+}
+
+const GameCard = (props: GameCardProps) => {
+  const { linkHref, linkTitle, imgSrc, imgAlt, gameTitle, scalVal } = props;
+  return (
+    <li className="border-0 border-green-400 group relative ">
+      <Link
+        href={linkHref}
+        className={`text-white block p-0 m-0  duration-300 ${scalVal ? `hover:scale-${scalVal}` : "hover:scale-110"}`}
+        title={linkTitle}
+      >
+        <Image src={imgSrc} alt={imgAlt} className="p-0 m-0 rounded-2xl" />
+      </Link>
+      <h2 className="absolute bottom-0 right-0 left-0  text-sm text-center font-bold text-white opacity-0 group-hover:opacity-100 transition-opacity duration-300 ">
+        {gameTitle}
+      </h2>
+    </li>
+  );
+};
+
+export default GameCard;
--- app/(HomePage)/page.tsx
+++ app/(HomePage)/page.tsx
@@ -10,6 +10,7 @@ import madalinImage from "@Public/madalin.jpg";
 import highwayRacerImage from "@Public/highway-racer-cover.png";
 import americanImage from "@Public/american-road-trip.png";
 import Slider from "react-slick";
+import GameCard from "app/(Games)/_GamesList/GameComponents/GameCard";
 
 // export const metadata: any = CustomMetaData({ presentURL: SEO_OBJ.HOME_PAGE.absoluteURL });
 
@@ -20,16 +21,22 @@ const Home = () => {
         <div className="firstMasonryGrid p-0 m-0">
           <h1 className="text-white text-2xl font-semibold">Trending Games</h1>
           <ol className="list-none grid grid-cols-9 gap-1 list-outside p-0 m-0">
-            <li className="border-0 border-green-400">
-              <Link href={"/racing"} className="text-white block p-0 m-0 ">
-                <Image src={escapeRoadImage} alt="escape-image" className="p-0 m-0 rounded-2xl" />
-              </Link>
-            </li>
-            <li className="border-0 border-green-400 ">
-              <Link href={"/escape-road"} className="text-white block p-0 m-0 ">
-                <Image src={madalinImage} alt="escape-image" className="p-0 m-0 rounded-2xl" />
-              </Link>
-            </li>
+            <GameCard
+              linkHref="/escape-road"
+              imgSrc={escapeRoadImage}
+              imgAlt="Escape-Road"
+              gameTitle="Escape Road "
+              linkTitle="Escape-Road"
+            />
+
+            <GameCard
+              linkHref="/escape-road"
+              imgSrc={highwayRacerImage}
+              imgAlt="Escape-Road"
+              gameTitle="Escape Road "
+              linkTitle="Escape-Road"
+            />
+
             <li className="border-0 border-green-400">
               <Link href={"/racing"} className="text-white block p-0 m-0 ">
                 <Image src={highwayRacerImage} alt="escape-image" className="p-0 m-0 rounded-2xl" />
@@ -377,28 +384,37 @@ const Home = () => {
           <h1 className="text-white text-2xl font-semibold">Car Games</h1>
           <ol className="slide-container list-none  list-outside p-0 m-0 w-full [&_li]:px-2 ">
             <Slider
-              autoplay={true}
+              autoplay={false}
               infinite={true}
               slidesToShow={6}
               rows={1}
               arrows={true}
               className="border-0 border-red-500 "
             >
-              <li className="border-0 border-green-400  ">
-                <Link href={"/escape-road"} className="text-white block p-0 m-0 ">
-                  <Image src={madalinImage} alt="escape-image" className="p-0 m-0 rounded-2xl " />
-                </Link>
-              </li>
-              <li className="border-0 border-green-400 ">
-                <Link href={"/escape-road"} className="text-white block p-0 m-0 ">
-                  <Image src={madalinImage} alt="escape-image" className="p-0 m-0 rounded-2xl" />
-                </Link>
-              </li>
-              <li className="border-0 border-green-400 row-span-2 col-span-2">
-                <Link href={"/racing"} className="text-white block p-0 m-0 ">
-                  <Image src={highwayRacerImage} alt="escape-image" className="p-0 m-0 rounded-2xl" />
-                </Link>
-              </li>
+              <GameCard
+                linkHref="/escape-road"
+                imgSrc={escapeRoadImage}
+                imgAlt="Escape-Road"
+                gameTitle="Escape Road "
+                linkTitle="Escape-Road"
+                scalVal={105}
+              />
+              <GameCard
+                linkHref="/escape-road"
+                imgSrc={escapeRoadImage}
+                imgAlt="Escape-Road"
+                gameTitle="Escape Road "
+                linkTitle="Escape-Road"
+                scalVal={105}
+              />
+              <GameCard
+                linkHref="/escape-road"
+                imgSrc={escapeRoadImage}
+                imgAlt="Escape-Road"
+                gameTitle="Escape Road "
+                linkTitle="Escape-Road"
+                scalVal={105}
+              />
               <li className="border-0 border-green-400">
                 <Link href={"/racing"} className="text-white block p-0 m-0 ">
                   <Image src={escapeRoadImage} alt="escape-image" className="p-0 m-0 rounded-2xl" />
